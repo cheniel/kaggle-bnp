4a68c67204aa4f1a767bb156765638d2c0a78d46
cheniel <danielchen627@gmail.com>
2016-03-06 20:23:31 -0500

    refactor boosted_trees for crossvalidation

--- boosted_trees.py
+++ boosted_trees.py
@@ -4,62 +4,61 @@ import xgboost as xgb
 from time import time
 from datetime import datetime
 
+
+def prepare_data(train_x, train_y):
+    return (xgb.DMatrix(train_x, label=train_y, missing=np.nan), train_y)
+
+
+def train_boosted_trees(train_x, train_y):
+
+    # setup parameters for xgboost
+    param = {}
+    param['objective'] = 'multi:softprob'
+    param['eta'] = 0.05
+    param['max_depth'] = 3
+    param['silent'] = 1
+    param['nthread'] = 4
+    param['num_class'] = 2
+    num_round = 5
+
+    return xgb.train(param, train_x, num_round)
+
+
+def predict_boosted_trees(bst, train_x):
+    return bst.predict(train_x)[:, 1]
+
+
 start = time()
 print 'Loading data...'
 train_data, test_data = load_data()
+xg_train, train_y = prepare_data(train_data['x'], train_data['y'])
+xg_test, _ = prepare_data(test_data['x'], None)
 print '...done loading data. Took {0} seconds'.format(time() - start)
-
-# todo: consider adding weights. esp re: 50
-# https://www.kaggle.com/bobcz3/bnp-paribas-cardif-claims-management/exploring-bnp-data-distributions
-
-start = time()
-print 'Loading DMatrix...'
-xg_train = xgb.DMatrix(train_data['x'], label=train_data['y'], missing=np.nan)
-xg_test = xgb.DMatrix(test_data['x'], missing=np.nan)
-print '...done loading DMatrix. Took {0} seconds'.format(time() - start)
-
-
-# setup parameters for xgboost
-param = {}
-# use softmax multi-class classification
-param['objective'] = 'multi:softprob'
-# scale weight of positive examples
-param['eta'] = 0.05
-param['max_depth'] = 6
-param['silent'] = 1
-param['nthread'] = 4
-param['num_class'] = 2
-
-watchlist = [ (xg_train,'train'), (xg_test, 'test') ]
-num_round = 300
 
 start = time()
 print 'Training model...'
-# bst = xgb.train(param, xg_train, num_round, watchlist )
-bst = xgb.train(param, xg_train, num_round)
+classifier = train_boosted_trees(xg_train, None)
 print '...model trained. Took {0} seconds'.format(time() - start)
 
 start = time()
+print 'Generating training predictions...'
+y_hat_train = predict_boosted_trees(classifier, xg_train)
+print '...predictions generated. Took {0} seconds'.format(time() - start)
+
+start = time()
 print 'Retrieving training error...'
-error = log_loss(train_data['y'], bst.predict( xg_train )[:, 1])
+error = log_loss(train_y, y_hat_train)
 print 'TRAINING ERROR: {}'.format(error)
 print '...Took {} seconds'.format(time() - start)
 
-# do the same thing again, but output probabilities
-# Note: this convention has been changed since xgboost-unity
-# get prediction, this is in 1D array, need reshape to (ndata, nclass)
 start = time()
-print 'Generating predictions on testing date...'
-yprob = bst.predict( xg_test )  # .reshape( test_['y'].shape[0], 6 )
-ylabel = np.argmax(yprob, axis=1)
+print 'Generating testing predictions...'
+y_hat_test = predict_boosted_trees(classifier, xg_test)
 print '...predictions generated. Took {0} seconds'.format(time() - start)
 
 start = time()
 print 'Writing output...'
 write_submission(
-    test_data['ids'], yprob[:, 1], 'boosted_trees-{}.csv'.format(datetime.now())
+    test_data['ids'], y_hat_test, 'boosted_trees-{}.csv'.format(datetime.now())
 )
 print '...output written. Took {0} seconds'.format(time() - start)
-
-
-# print ('predicting, classification error=%f' % (sum( int(ylabel[i]) != test_Y[i] for i in range(len(test_Y))) / float(len(test_Y)) ))
